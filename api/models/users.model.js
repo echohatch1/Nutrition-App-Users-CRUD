9ab313916891e8a642289cce25bf3b4f38a2b14c
Landon Hatch <landonhatch@gmail.com>
2019-11-30 15:46:40 -0700

    Swapped first and last for username

--- api/models/users.model.js
+++ api/models/users.model.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 
 
 const UsersSchema = new Schema({
-    first: { type: String, lowercase: true, required: true, max: 100 },
-    last: { type: String, lowercase: true, required: true, max: 100 },
+    username: { type: String, lowercase: true, required: true, max: 100 },
     email: { type: String, lowercase: true, required: true, max: 100 },
     date: { type: Date, default: Date.now },
     foods: [{type: Object}],
