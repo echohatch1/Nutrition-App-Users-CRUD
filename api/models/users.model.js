ec1a7c33f4a53a4e8576c5eb051e97502932fa7c
Landon Hatch <landonhatch@gmail.com>
2019-11-29 17:09:52 -0700

    Added timestamp

--- api/models/users.model.js
+++ api/models/users.model.js
@@ -7,7 +7,7 @@ const UsersSchema = new Schema({
     first: { type: String, lowercase: true, required: true, max: 100 },
     last: { type: String, lowercase: true, required: true, max: 100 },
     email: { type: String, lowercase: true, required: true, max: 100 },
-    time : { type : Date, default: Date.now },
+    date: { type: Date, default: Date.now },
     food: { type: Object },
 });
 
